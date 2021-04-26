862609ad9b6db6e0962575ab2531dcca805170a3
James V <james.vanhoke@yahoo.com>
2021-04-25 20:44:01 -0700

    updated service worker with new cached files

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,75 +1,43 @@
+// Designate what we want to cache, usually everything in the public folder
+
 const FILES_TO_CACHE = [
   "/",
-  "/index.html",
-  "/assets/css/style.css",
-  "/dist/bundle.js",
-  "/assets/images/1.jpg",
-  "/assets/images/2.jpg",
-  "/assets/images/3.jpg",
-  "/assets/images/4.jpg",
-  "/assets/images/5.jpg",
-  "/assets/images/6.jpg",
-  "/assets/images/7.jpg",
-  "/assets/images/8.jpg",
-  "/assets/images/9.jpg",
-  "/assets/images/10.jpg",
-  "/assets/images/11.jpg",
-  "/assets/images/12.jpg",
-  "/assets/images/13.jpg",
-  "/assets/images/14.jpg",
-  "/assets/images/15.jpg",
-  "/assets/images/16.jpg",
-  "/assets/images/17.jpg",
-  "/assets/images/18.jpg",
-  "/assets/images/19.jpg",
-  "/assets/images/20.jpg",
-  "/assets/images/21.jpg",
-  "/assets/images/22.jpg",
-  "/assets/images/23.jpg",
-  "/assets/images/24.jpg",
-  "/assets/images/25.jpg",
-  "/assets/images/26.jpg",
-  "/assets/images/27.jpg",
-  "/assets/images/28.jpg",
-  "/assets/images/29.jpg",
-  "/assets/images/30.jpg",
-  "/assets/images/31.jpg",
-  "/assets/images/32.jpg",
-  "/assets/images/33.jpg",
-  "/assets/images/34.jpg",
-  "/assets/images/35.jpg",
-  "/assets/images/36.jpg",
-  "/assets/images/37.jpg",
-  "/assets/images/38.jpg"
-];
+  "./index.html",
+  "./indexdb.js",
+  "./styles.css",
+  "./index.js",
+  "./icons/icon-192x192.png",
+  "./icons/icon-512x512.png",
+  "./manifest.json"
+]
 
-const STATIC_CACHE = "static-cache-v1";
-const RUNTIME_CACHE = "runtime-cache";
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
 
-self.addEventListener("install", event => {
-  event.waitUntil(
+// Cache files on service worker install
+self.addEventListener("install", (e) => {
+  e.waitUntil(
     caches
-      .open(STATIC_CACHE)
-      .then(cache => cache.addAll(FILES_TO_CACHE))
+      .open(CACHE_NAME)
+      .then((cache) => cache.addAll(FILES_TO_CACHE))
       .then(() => self.skipWaiting())
   );
 });
 
-// The activate handler takes care of cleaning up old caches.
-self.addEventListener("activate", event => {
-  const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
-  event.waitUntil(
+// Clears unneeded cache files
+self.addEventListener("activate", (e) => {
+  const currentCaches = [CACHE_NAME, DATA_CACHE_NAME];
+  e.waitUntil(
     caches
       .keys()
-      .then(cacheNames => {
-        // return array of cache names that are old to delete
+      .then((cacheNames) => {
         return cacheNames.filter(
-          cacheName => !currentCaches.includes(cacheName)
+          (cacheName) => !currentCaches.includes(cacheName)
         );
       })
-      .then(cachesToDelete => {
+      .then((cachesToDelete) => {
         return Promise.all(
-          cachesToDelete.map(cacheToDelete => {
+          cachesToDelete.map((cacheToDelete) => {
             return caches.delete(cacheToDelete);
           })
         );
@@ -78,43 +46,39 @@ self.addEventListener("activate", event => {
   );
 });
 
-self.addEventListener("fetch", event => {
-  // non GET requests are not cached and requests to other origins are not cached
+
+self.addEventListener("fetch", (e) => {
   if (
-    event.request.method !== "GET" ||
-    !event.request.url.startsWith(self.location.origin)
+    e.request.method !== "GET" ||
+    !e.request.url.startsWith(self.location.origin)
   ) {
-    event.respondWith(fetch(event.request));
+    e.respondWith(fetch(e.request));
     return;
   }
 
-  // handle runtime GET requests for data from /api routes
-  if (event.request.url.includes("/api/images")) {
-    // make network request and fallback to cache if network request fails (offline)
-    event.respondWith(
-      caches.open(RUNTIME_CACHE).then(cache => {
-        return fetch(event.request)
-          .then(response => {
-            cache.put(event.request, response.clone());
+  if (e.request.url.includes("/api/images")) {
+    e.respondWith(
+      caches.open(DATA_CACHE_NAME).then((cache) => {
+        return fetch(e.request)
+          .then((response) => {
+            cache.put(e.request, response.clone());
             return response;
           })
-          .catch(() => caches.match(event.request));
+          .catch(() => caches.match(e.request));
       })
     );
     return;
   }
 
-  // use cache first for all other requests for performance
-  event.respondWith(
-    caches.match(event.request).then(cachedResponse => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
       if (cachedResponse) {
         return cachedResponse;
       }
 
-      // request is not in cache. make network request and cache the response
-      return caches.open(RUNTIME_CACHE).then(cache => {
-        return fetch(event.request).then(response => {
-          return cache.put(event.request, response.clone()).then(() => {
+      return caches.open(DATA_CACHE_NAME).then((cache) => {
+        return fetch(e.request).then((response) => {
+          return cache.put(e.request, response.clone()).then(() => {
             return response;
           });
         });
